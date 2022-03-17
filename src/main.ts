754d03c2afd44c5ed2d377112ff3cc430126631c
luc.nq <quocluc97@gmail.com>
2022-03-17 14:24:28 +0700

    update port use in env

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
